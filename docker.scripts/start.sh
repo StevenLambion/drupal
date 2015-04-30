22094f0f07eb62913c3593c336cbfdcedab4b73d
Steven Lambion <steven.lambion@atsid.com>
2015-04-29 22:23:29 -0700

    Add start to the NWCAA install profile

--- docker.scripts/start.sh
+++ docker.scripts/start.sh
@@ -17,7 +17,7 @@ if [ ! -f /var/www/sites/default/settings.php ]; then
 	sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/sites-available/default
 	a2enmod rewrite vhost_alias
 	cd /var/www/
-	drush site-install standard -y --account-name=admin --account-pass=admin --db-url="mysqli://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}" --debug
+	drush site-install nwcaa -y --account-name=admin --account-pass=admin --db-url="mysqli://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}" --debug
 	# killall mysqld
 	sleep 10s
 fi
